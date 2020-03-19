4b3f3abf5caf7a990bb0a035123378ae09c7c4ef
Wilco Louwerse <wilco@louwerse.com>
2020-03-19 14:37:08 +0100

    Added project with empty application
    version 0.1

---
+++ begrafenisplanner/begrafenisplanner/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace begrafenisplanner.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
